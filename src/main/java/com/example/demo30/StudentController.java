fc1c76fa8943ff827e14ee732df7a81edbf7ebf0
Howard Chang <howard68790@gmail.com>
2024-01-30 14:10:11 +0800

    use spring data jpa crud

--- src/main/java/com/example/demo30/StudentController.java
+++ src/main/java/com/example/demo30/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo30;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
@@ -13,6 +11,33 @@ public class StudentController {
     @PostMapping("/students")
     public String insert(@RequestBody Student student) {
         studentRepository.save(student);
+
         return "執行資料庫的 create 操作";
+    }
+
+    @PutMapping("/students/{studentId}")
+    public String update(@PathVariable Integer studentId,
+                         @RequestBody Student student) {
+        Student s = studentRepository.findById(studentId).orElse(null);
+
+        if (s != null) {
+            s.setName(student.getName());
+            studentRepository.save(student);
+
+            return "執行資料庫的 update 操作";
+        } else {
+            return "not found";
+        }
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public String delete(@PathVariable Integer studentId) {
+        studentRepository.deleteById(studentId);
+        return "執行資料庫的 delete 操作";
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student read(@PathVariable Integer studentId) {
+        return studentRepository.findById(studentId).orElse(null);
     }
 }
--- src/main/java/com/example/demo30/StudentRepository.java
+++ src/main/java/com/example/demo30/StudentRepository.java
@@ -2,4 +2,5 @@ package com.example.demo30;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentRepository extends CrudRepository<Student, Integer> {}
+public interface StudentRepository extends CrudRepository<Student, Integer> {
+}
